5ca181e515f3b8c5dc9b6c5223099a8ee9b7c060
Suraj Sharma <rishishwarsuraj@gmail.com>
2024-10-19 12:24:46 +0530

    Day_2 Playing with ENV Variables

--- src/main/java/com/Suraj/demo/controller/Controller.java
+++ src/main/java/com/Suraj/demo/controller/Controller.java
@@ -1,5 +1,7 @@
 package com.Suraj.demo.controller;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/hello")
 public class Controller {
+
+    @Value("${app.myname}")
+    private String appName;
     @GetMapping
     public String hello(){
-        return "This is under Development.....Visit again!!! :)";
+
+        return appName;
     }
 }
